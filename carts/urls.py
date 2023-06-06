39eec6ccb632ebecc4bdd712f3a82fb8dce670f4
arpan <ingnamarpan@gmail.com>
2023-06-06 20:17:59 +0545

    Update urls.py

--- carts/urls.py
+++ carts/urls.py
@@ -5,10 +5,10 @@ from . import views
 app_name = "carts"
 urlpatterns = [
     path("", views.cart, name="cart"),
-    path("add-to-cart/<int:product_id>/", views.add_to_cart, name="add_to_cart"),
-    path("remove-cart/<int:product_id>/", views.remove_cart, name="remove_cart"),
+    path("add/<int:product_id>/", views.add_to_cart, name="add_to_cart"),
+    path("remove/<int:product_id>/", views.remove_cart, name="remove_cart"),
     path(
-        "remove-cart-item/<int:product_id>/",
+        "remove/cart-item/<int:product_id>/",
         views.remove_cart_item,
         name="remove_cart_item",
     ),
