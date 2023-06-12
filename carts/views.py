657b877d66f94291b77fbc4c80cc36c74ab2bdad
arpan <ingnamarpan@gmail.com>
2023-06-12 10:03:54 +0545

    Added checkout page

--- carts/views.py
+++ carts/views.py
@@ -121,3 +121,27 @@ def remove_cart_item(request, product_id, cart_item_id):
     cart_item = CartItem.objects.get(id=cart_item_id, product=product, cart=cart)
     cart_item.delete()
     return redirect("carts:cart")
+
+
+def checkout_view(request, total=0, total_quantity=0, cart_items=None):
+    try:
+        tax = 0
+        grand_total = 0
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+        cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+        for cart_item in cart_items:
+            total += cart_item.product.price * cart_item.quantity
+            total_quantity += cart_item.quantity
+        tax = (2 * total) / 100  # 2% tax on total amount
+        grand_total = total + tax
+    except ObjectDoesNotExist:
+        pass
+
+    context = {
+        "total": total,
+        "total_quantity": total_quantity,
+        "cart_items": cart_items,
+        "tax": tax,
+        "grand_total": grand_total,
+    }
+    return render(request, "carts/checkout.html", context)
